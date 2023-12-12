12097984c201d093610561681ccfb50646b0b665
Dwayne Rodrigues <dwayne.rodrigues@hotmail.ca>
2023-12-12 17:29:14 -0500

    added scroll event listener

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+document.querySelector("#scroll-down").addEventListener("click", () => {
+  window.scrollTo({
+    top: document.querySelector("#about-me").offsetTop - 20,
+  });
+});
+
 document.querySelector("#toggle-theme").addEventListener("click", () => {
   document.documentElement.classList.toggle("light-theme");
 });
